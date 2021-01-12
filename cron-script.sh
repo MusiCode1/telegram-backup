50ab4bdcab8faec81066a797f51049b74fb1df7d
AviBritman <aybritman@gmail.com>
2021-01-12 23:10:51 +0200

    .

--- cron-script.sh
+++ cron-script.sh
@@ -1,6 +1,13 @@
 # /bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+# export PATH=/usr/local/bin:$PATH
+export ~/.local/bin/pipenv
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 
+SCRIPTPATH="$(
+    cd "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)"
 cd $SCRIPTPATH
 
 ~/.local/bin/pipenv shell python main.py
